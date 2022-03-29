be750e3604331589dbebbac20378b1cc44d1e800
sumit7577 <asmitkumarr082@gmail.com>
2022-03-29 19:37:20 +0530

    added products taking

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { Shopify } from "@shopify/shopify-api";
 
 dotenv.config();
 
+//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
 const app = express();
 const port = 3000;
 
@@ -15,8 +16,8 @@ Shopify.Context.initialize(
     {
         API_KEY: SHOPIFY_API_KEY,
         API_SECRET_KEY: SHOPIFY_API_SECRET,
-        SCOPES: [SHOPIFY_API_SCOPES],
-        HOST_NAME:"a384-2402-8100-263e-8151-796c-565-f2be-f79e.ngrok.io",
+        SCOPES: SHOPIFY_API_SCOPES,
+        HOST_NAME:"5490-2402-8100-263c-40ed-25bf-c8a5-125f-4866.ngrok.io",
         IS_EMBEDDED_APP: true,
     }
 )
@@ -25,7 +26,7 @@ app.get('/', async (req, res) => {
         res.send("hllo world");
     }
     else {
-        res.redirect(`/auth?shop=${req.query.shop}`);
+        res.redirect(`/auth?shop=brotherauth.myshopify.com`);
     }
 });
 
@@ -44,15 +45,21 @@ app.get("/auth/callback", async (req, res) => {
     const shopSession = Shopify.Auth.validateAuthCallback(
         req,
         res,
-        req.query
+        req.query,
     );
     const data = (await shopSession).shop;
     const token = (await shopSession).accessToken;
-    res.send(token);
     shops[data] = shopSession;
+    const makeRequest = await fetch("https://brotherauth.myshopify.com/admin/api/2021-07/products.json".{
+        method:"GET",
+        headers:{
+            'Content-Type': 'application/json',
+            'X-Shopify-Access-Token':token,
+        }
+    });
+    const data = await makeRequest.json();
+    res.send(data.products);
 })
-
-
 
 
 
